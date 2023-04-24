e14ed1839aed570c74cfd43944856915fdcd7cb2
einevi <117112903+einevi@users.noreply.github.com>
2023-04-24 14:45:16 +0300

    first commit

---
+++ Daire.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Daire {
+    public static void main(String[] args) {
+        int r, a;
+        double pi = 3.14;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Üçgenin yarı çapını giriniz : ");
+        r = input.nextInt();
+        System.out.println("Üçgenin alanı = " + pi * r * r);
+        System.out.println("Üçgenin çevresi = " + pi * r * 2);
+
+    }
+}
