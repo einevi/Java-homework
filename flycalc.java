6f345cae9917f9cc21de21076f84b42a1d741cc2
emre can inevi <117112903+einevi@users.noreply.github.com>
2023-06-14 15:12:23 +0300

    Update flycalc.java

--- flycalc.java
+++ flycalc.java
@@ -3,33 +3,52 @@ public class flycalc {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
 
-        double mile, age, total;
+        double km, costPerKm = 0.1 , total;
+        byte type;
+        int age;
         boolean isError = false;
-        boolean type = true;
 
-        System.out.print("Mesafeyi km türünden giriniz : ");
-        mile = inp.nextInt();
+        System.out.print("Mesafeyi dahil ediniz : ");
+        km = inp.nextDouble();
+
         System.out.print("Yaşınızı giriniz : ");
         age = inp.nextInt();
+
         System.out.print("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): ");
-        type = inp.nextBoolean();
-        
-            System.out.print("Hatalı Giriş Yaptınız.\nYolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): ");
+        type = inp.nextByte();
+        total = km * costPerKm;
+
+        if (age<12){
+            total *= 0.5;
+        }
+        else if (age>=12 && age<=24) {
+            total *= 0.9;
+        }
+        else if (age>65) {
+            total *= 0.7;
         }
 
-        if (age < 12){
-            total = ((mile * 0.10) / 0.50);
-            System.out.println("Toplam Tutar : " + total);
+        switch (type){
+            case 2:
+                total *= 0.8;
+                total *= 2;
+                break;
+            case 1:
+                total = total;
+                break;
+            default:
+                isError = true;
         }
 
-        else if (12 >= age && age >= 24) {
-            total = ((mile * 0.10) / 0.10);
-            System.out.println("Toplam Tutar : " + total);
+        if (!(km>=0)){
+            isError = true;
         }
 
-        else if (age > 65) {
-            total = ((mile * 0.10) / 0.30);
-            System.out.println("Toplam Tutar : " + total);
+        if (isError == true){
+            System.out.println("Hatalı Veri Girdiniz !");
         }
-
-}}
+        else{
+            System.out.println("Toplam Tutar: " + total + " TL");
+        }
+    }
+}
